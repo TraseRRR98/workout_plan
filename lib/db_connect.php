1af80d1de52ce565704bee1598ff22ba4a8f2cb8
TraseRRR98 <traserrr@gmail.com>
2024-10-31 19:54:08 -0400

    Added database connection script

---
+++ lib/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+    // Define Database Credentials
+    define('DB_SERVER', '127.0.0.1');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'workout_plan');
+    
+    global $conn;
+    // Attempt To Connect To MySQL Database
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    
+    // Verify Connection
+    if ($conn === false) {
+        die("ERROR: Could not connect: " . mysqli_connect_error());
+    }
+?>

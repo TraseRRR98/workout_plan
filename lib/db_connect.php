65d3e27848c063c32aaf59f8576001f0e4527735
TraseRRR98 <traserrr@gmail.com>
2024-11-19 18:38:23 -0500

    Update database name in connection script to 'workout_tracker'

--- lib/db_connect.php
+++ lib/db_connect.php
@@ -3,14 +3,13 @@
     define('DB_SERVER', '127.0.0.1');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
-    define('DB_NAME', 'workout_plan');
+    define('DB_NAME', 'workout_tracker');
     
     global $conn;
     // Attempt To Connect To MySQL Database
     $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
     
     // Verify Connection
-    if ($conn === false) {
+    if ($conn === false) 
         die("ERROR: Could not connect: " . mysqli_connect_error());
-    }
 ?>
